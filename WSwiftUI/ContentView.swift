d899e7bc461f4e62f2bc807ab5578acd9e9255d7
1002659 <baw0803@sk.com>
2021-03-22 12:31:14 +0900

    [DEV] init

---
+++ WSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WSwiftUI
+//
+//  Created by wooky83 on 2021/03/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+            Image(systemName: "swift")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WSwiftUI/WSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WSwiftUIApp.swift
+//  WSwiftUI
+//
+//  Created by wooky83 on 2021/03/17.
+//
+
+import SwiftUI
+
+@main
+struct WSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
