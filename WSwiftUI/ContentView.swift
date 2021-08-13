4f1d84cd83fee7bf7e22c26dafad03e4446c405b
sungwookKwon <sw.kwon7@cj.net>
2021-08-13 15:06:39 +0900

    [Dev] SwiftUI Basic Code

--- WSwiftUI/ContentView.swift
+++ WSwiftUI/ContentView.swift
@@ -10,8 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hello, world!")
             Image(systemName: "swift")
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.system(size: 14))
+            Text("89")
+                .kerning(-1)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack {
+                Text("1")
+                    .bold()
+                Slider(value: .constant(50.0), in: 1.0...100.0)
+                Text("100")
+                    .bold()
+            }
+            Button(action: {}) {
+                Text("Hit me")
+            }
         }
     }
 }
@@ -19,5 +38,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
     }
 }
