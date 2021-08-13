772dd9663db598953172aff9501c0f309603a067
sungwookKwon <sw.kwon7@cj.net>
2021-08-13 15:20:10 +0900

    [Dev] SwiftUI LifeCycle

---
+++ WSwiftUI/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  WSwiftUI
+//
+//  Created by sungwook on 2021/08/13.
+//
+
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("MyAppDelegate didFinishLaunchingWithOptions called")
+        return true
+    }
+}
--- WSwiftUI/WSwiftUIApp.swift
+++ WSwiftUI/WSwiftUIApp.swift
@@ -9,9 +9,35 @@ import SwiftUI
 
 @main
 struct WSwiftUIApp: App {
+    @Environment(\.scenePhase) var scenePhase
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
+    init() {
+        /// perform Any task on app launch
+        print("Perform any task here in init")
+    }
+    
     var body: some Scene {
         WindowGroup {
+            
             ContentView()
+                .onOpenURL(perform: { url in
+                print("Incoming url: \(url)")
+            })
+            
+        }
+        .onChange(of: scenePhase) { (newScenePhase) in
+            switch newScenePhase {
+            case .background:
+                print("App State : Background")
+            case .inactive:
+                print("App State : Inactive")
+            case .active:
+                print("App State : Active")
+            @unknown default:
+                print("App State : Unknown")
+            }
         }
     }
 }
